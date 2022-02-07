3c5cb5559630740739d080f6cbdb416a84dd6d1a
Juliano Rossi <juliano.rossi@me.com>
2022-02-06 22:46:16 -0300

    Fully working UseReducer and UseContext

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './Cart.module.css';
+import CartContext from '../../store/CartContext';
 
 const Cart = (props) => {
+  const context = useContext(CartContext);
+  console.log('state do cart é:');
+  console.log(context.state);
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => {
-        return <li key={Math.random}>{item.name}</li>;
+      {context.state.items.map((item) => {
+        return <li key={Math.random()}>{item.name}</li>;
       })}
     </ul>
   );
@@ -14,7 +18,7 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{context.state.totalAmount.toFixed(2)}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClick}>
